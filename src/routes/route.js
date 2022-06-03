ad0f6165ce1fb97af83a568092462d7513fc4743
Aqueeb99 <enggaqueeb@gmail.com>
2022-06-04 01:30:25 +0530

    Full assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,21 @@
 const express = require('express');
-// const myHelper = require('../util/helper')
-// const formatter = require('../validator/formatter')
+ const myHelper = require('../util/helper')
+ const formatter = require('../validator/formatter')
 const x = require('lodash');
-// const underscore = require('underscore')
+ const underscore = require('underscore')
  const router = express.Router();
+ 
 
-// router.get('/test-me', function (req, res) {
-//      myHelper.printDate()
-//      myHelper.printMonth() 
-//     myHelper.getBatchInfo()
-//      formatter.trim()
-//      formatter.lower()
-//     formatter.upper()
-//     res.send('My first ever API')
+router.get('/test-me', function (req, res) {
+     myHelper.printDate()
+     myHelper.printMonth() 
+    myHelper.getBatchInfo()
+     formatter.trim()
+     formatter.lower()
+    formatter.upper()
+    res.send('My first ever API')
   
-//});
+});
 
  router.get('/hello', function (req, res) {
             
@@ -31,9 +32,39 @@ const x = require('lodash');
 
         let pairs = [["horror","The Shining"], ["drama","Titanic"] ,["thriller","Shutter Island"] ,["fantasy","Pans Labyrinth"]]
         console.log(x.fromPairs(pairs))
-        
+
      res.send('Hello there')
  });
 
+
+router.get("/sol1", function (req, res) {
+    logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    let missingNumber
+    let len = arr.length
+    let sum = 0
+for (let i=0; i<len; i++) {
+sum = sum + arr[i]
+}
+sum1 = ((len+1)*(len+2))/2
+missingNumber = sum1 - sum
+    res.send(  { data: missingNumber  } ) 
+     });
+
+     router.get("/sol2", function (req, res) {
+        let arr2= [33, 34, 35, 37, 38]
+        let missingNumber
+        let len = arr2.length
+        let sum2 = 0
+        let a = arr2[0]
+        let l = arr2.pop()
+    for (let i=0; i<len; i++) {
+    sum2 = sum2 + arr2[i]
+    }
+    sum3 = ((len+1)*(a+l))/2
+    missingNumber = sum3 - sum2
+        res.send(  { data: missingNumber  } ) 
+         });
+     
     module.exports = router;
 // adding this comment for no reason
